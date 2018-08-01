ab074829d308a2da5692f063818443a1ec4df86c
Colin Kushniruk <colinkushniruk@gmail.com>
2018-07-31 22:22:57 -0600

    Set up build bash script to convert main php files to html, set up netlify toml to declare bash script build command. Changed htaccess and index.php to replace file paths to proper conversions.

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php 
+
+	$name = trim( explode( '?', $_SERVER['REQUEST_URI'], 2 )[0], '/' );
+	$name = str_replace( '.', '', $name );
+	$file = '404';
+
+	if ( $name !== 'index' && is_file( __dir__ . "/$name.php" ) ) {
+		$file = $name;
+	} elseif (!$name) {
+		$file = 'home';
+	}
+
+	$dir = dirname(__FILE__);
+	include $dir . "/partials/header.php";
+	require $dir . "/$file.php";
+	include $dir . "/partials/footer.php";
+
 	session_start();
 	require_once ('includes/config.php');
 	require_once ('includes/classes/website.class.php');
